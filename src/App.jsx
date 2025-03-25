f157299423eaf7c813a014a5160f26e78447bff4
anikatasnin <jarintasnin09@gmail.com>
2025-03-25 20:08:20 +0600

    fetched users data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
 import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
   return (
     <>
       <h1>Users Management System</h1>
