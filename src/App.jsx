986ebd5e6e11c22f1456cca6e1526ee40bfda2bb
anikatasnin <jarintasnin09@gmail.com>
2025-03-25 23:42:07 +0600

    post user data

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,17 @@ function App() {
     const user = { name, email };
 
     console.log(user);
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("inside post response", data);
+      });
   };
 
   return (
